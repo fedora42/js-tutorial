b38345834284cea887b599a2572c48083461351b
Shashank <shashankpadhye9@gmail.com>
2025-01-22 08:52:58 +0000

    Variables update

---
+++ Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 23451
+let accountName = "shashank"
+accountEmail = "shashankpadhye9@gmail.com"
+var accountCity = "Delhi"
+let accountState;
+accountEmail = "satyam@gmail.com"
+accountCity = "Pune"
+
+// Prefer not use var beacuse it doesnot surpport the scope of the bock {}
+
+console.table([accountId,accountName,accountCity,accountEmail,accountState])
