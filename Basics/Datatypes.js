49373b6bdf4ffa1c0af89536ca3aa067ba247e8f
Shashank <shashankpadhye9@gmail.com>
2025-01-22 10:40:42 +0000

    added datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treats all JS code as newer version of javascript
+let age = 21
+let name = "shashank"
+let something = null
+let isLoggedIn = false
+let shashank;
+
+
+//symbol => unique
+console.log(typeof something)
+
+
+//null is an object but undefined is a type itself 
+console.log(typeof shashank)
